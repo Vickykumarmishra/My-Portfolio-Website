5e11a73459c287ab084bb484468faae25bd879ae
vicky kumar mishra <mishravicky0141@gmail.com>
2023-09-15 10:37:55 +0530

    AOS ADDED

--- My-portfolio-website-using-React-js/myportfolio/src/components/Home.js
+++ My-portfolio-website-using-React-js/myportfolio/src/components/Home.js
@@ -17,7 +17,7 @@ export default function Home() {
          transition={{delay:0.4,duration:5,repeat:0}}style={{color:'white',fontSize:'5rem',fontFamily:'tahoma',textShadow: '1px 1px 2px black, 0 0 25px yellow, 0 0 5px yellow'}}>Vicky Kumar Mishra</motion.h1>
         <center>
         <div style={{marginRight:'5rem',marginLeft:'5rem'}} className='shadow-lg p-3 mb-5 bg-black rounded'>
-        <p style={{color:'lightgreen',fontFamily:'tahoma',textShadow: '1px 1px 2px black, 0 0 25px white, 0 0 5px white'}}>
+        <p style={{color:'lightgreen',fontFamily:'tahoma'}}>
         Welcome to my portfolio! I'm a frontend developer specializing in creating engaging and user-friendly web experiences. With expertise in HTML, CSS, and JavaScript, I transform ideas into functional websites using modern framework like React Js . 
         My portfolio showcases diverse projects that highlight my attention to detail and passion for intuitive interfaces. Let's collaborate to bring your vision to life.
 
@@ -36,19 +36,19 @@ export default function Home() {
 
 
     <div className="container text-center" style={{marginBottom:'2rem'}}>
-  <div className="row">
+  <div className="row" data-aos="fade-up"  data-aos-duration="1500">
     <div className="col" style={{marginTop:'1rem',marginBottom:'2rem'}}>
     
 
     <h3 style={{fontSize:"2rem", color:'white',textShadow: '1px 1px 2px black, 0 0 25px lightgreen, 0 0 10px lightgreen',marginBottom:"2rem"}}>My Skills:</h3>
-    <div className='container'>
-    <motion.img whileHover={{scale:'1.1'}} src='html-5.png' style={{height:'5rem'}}></motion.img>
-    <motion.img src='csss.png' whileHover={{scale:'1.1'}}  style={{height:'5rem'}}></motion.img>
-    <motion.img src='java-script.png' whileHover={{scale:'1.1'}}  style={{height:'5rem'}}></motion.img>
-    <motion.img src='logo192.png' whileHover={{scale:'1.1'}} style={{height:'5rem'}}></motion.img>
-    <motion.img src='bootstrap.png' whileHover={{scale:'1.1'}}   style={{height:'5rem'}}></motion.img>
-    <motion.img src="c++.png" whileHover={{scale:'1.1'}}  style={{height:'5rem',marginLeft:'0.4rem'}}></motion.img>
-    <motion.img src='redhux.svg' whileHover={{scale:'1.1'}}  style={{height:'5rem',marginLeft:'0.4rem'}}></motion.img></div>
+    <div className='container' >
+    <img whileHover={{scale:'1.1'}} src='html-5.png' style={{height:'5rem'}}></img>
+    <img src='csss.png' whileHover={{scale:'1.1'}}  style={{height:'5rem'}}></img>
+    <img src='java-script.png' whileHover={{scale:'1.1'}}  style={{height:'5rem'}}></img>
+    <img src='logo192.png' whileHover={{scale:'1.1'}} style={{height:'5rem'}}></img>
+    <img src='bootstrap.png' whileHover={{scale:'1.1'}}   style={{height:'5rem'}}></img>
+    <img src="c++.png" whileHover={{scale:'1.1'}}  style={{height:'5rem',marginLeft:'0.4rem'}}></img>
+    <img src='redhux.svg' whileHover={{scale:'1.1'}}  style={{height:'5rem',marginLeft:'0.4rem'}}></img></div>
     {/* <h3 style={{fontSize:"2em", color:'white',textShadow: '1px 1px 2px black, 0 0 25px lightgreen, 0 0 10px lightgreen'}}>My Skills:</h3>
     <center><motion.table whileHover={{scale:1.1}} style={{color:'white',textShadow: '1px 1px 2px black, 0 0 25px lightgreen, 0 0 20px lightgreen'}}>
     
@@ -127,11 +127,8 @@ export default function Home() {
     <div class="row" >
     <div class="col" >
       <NavLink to='https://tourism-web-with-react-by-vicky.vercel.app/'><motion.h3  whileHover={{color:'yellow'}}  transition={{duration:1 ,repeat:Infinity}} style={{marginTop:'2rem',color:'lightgreen'}}>Tourism Website</motion.h3></NavLink>
-     <NavLink to='https://tourism-web-with-react-by-vicky.vercel.app/'><motion.img whileHover={{scale:1.1,transform:'rotatey(180deg)'}} transition={{duration:1}} src='buddha.jpg' className='img-thumbnail' style={{marginTop:'3rem',height:'20rem',width:'30rem',boxShadow:'1px 1px 2px black, 0 0 25px white, 0 0 5px white'}} alt='....'/></NavLink>
+     <NavLink to='https://tourism-web-with-react-by-vicky.vercel.app/'><img data-aos="fade-up" data-aos-duration="1000" src='buddha.jpg' className='img-thumbnail' style={{marginTop:'3rem',height:'20rem',width:'30rem',boxShadow:'1px 1px 2px black, 0 0 25px white, 0 0 5px white'}} alt='....'/></NavLink>
      
-      <div className="col" style={{marginTop:'1rem'}}>
-     
-    </div>
     </div>
   </div>
     </div>
@@ -139,7 +136,7 @@ export default function Home() {
     <div class="row">
     <div class="col" >
       <NavLink to='https://typing-web-by-vicky.netlify.app/'><motion.h3  whileHover={{color:'yellow'}}  transition={{duration:1 ,repeat:Infinity}} style={{marginTop:'2rem',color:'lightgreen'}}>Typing Website</motion.h3></NavLink>
-     <NavLink to='https://typing-web-by-vicky.netlify.app/'><motion.img whileHover={{scale:1.1,transform:'rotatey(180deg)'}} transition={{duration:1}}src='typist.jpg' className='img-thumbnail' style={{marginTop:'3rem',height:'20rem',boxShadow:'1px 1px 2px black, 0 0 25px white, 0 0 5px white'}} alt='....'/></NavLink>
+     <NavLink to='https://typing-web-by-vicky.netlify.app/'><img data-aos="fade-up" data-aos-duration="1000" src='typist.jpg' className='img-thumbnail' style={{marginTop:'3rem',height:'20rem',boxShadow:'1px 1px 2px black, 0 0 25px white, 0 0 5px white'}} alt='....'/></NavLink>
      
       <div className="col" style={{marginTop:'1rem'}}>
       
@@ -153,7 +150,7 @@ export default function Home() {
     <div class="row">
     <div class="col" >
       <NavLink to='https://takcart-my-shopping-app.netlify.app'><motion.h3  whileHover={{color:'yellow'}}  transition={{duration:1 ,repeat:Infinity}} style={{marginTop:'2rem',color:'lightgreen'}}>Shopping website</motion.h3></NavLink>
-     <NavLink to='https://takcart-my-shopping-app.netlify.app'><motion.img whileHover={{scale:1.1,transform:'rotatey(180deg)'}} transition={{duration:1 }} src='takcart portfolio.jpg' className='img-thumbnail' style={{marginTop:'3rem',height:'20rem',boxShadow:'1px 1px 2px black, 0 0 25px white, 0 0 5px white'}} alt='....'/></NavLink>
+     <NavLink to='https://takcart-my-shopping-app.netlify.app'><img data-aos="fade-up" data-aos-duration="1000" src='takcart portfolio.jpg' className='img-thumbnail' style={{marginTop:'3rem',height:'20rem',boxShadow:'1px 1px 2px black, 0 0 25px white, 0 0 5px white'}} alt='....'/></NavLink>
      
       <div className="col" style={{marginTop:'1rem'}}>
       
