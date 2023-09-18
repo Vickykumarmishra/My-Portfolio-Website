80fed3a828c712bb66d598f19b9e07ce2316ea5d
vicky kumar mishra <mishravicky0141@gmail.com>
2023-09-19 00:21:40 +0530

    Update Home.js

--- My-portfolio-website-using-React-js/myportfolio/src/components/Home.js
+++ My-portfolio-website-using-React-js/myportfolio/src/components/Home.js
@@ -13,6 +13,7 @@ export default function Home() {
     imageWidth: 400,
     imageHeight: 200,
     imageAlt: 'Custom image',
+    color:'red',
   })
 
  }
@@ -48,7 +49,7 @@ export default function Home() {
 
     <div className="container text-center" style={{marginBottom:'2rem'}}>
   <div className="row" data-aos="fade-up"  data-aos-duration="1500">
-    <div className="col" style={{marginTop:'1rem',marginBottom:'2rem'}}>
+    <div className="col" style={{marginTop:'1rem',marginBottom:'2rem' }}>
     
 
     <h3 style={{fontSize:"2rem", color:'white',textShadow: '1px 1px 2px black, 0 0 25px lightgreen, 0 0 10px lightgreen',marginBottom:"2rem"}}>My Skills:</h3>
@@ -88,7 +89,7 @@ export default function Home() {
     <div class="col">
     <div class="row" >
     <div class="col" >
-      <NavLink to='https://tourism-web-with-react-by-vicky.vercel.app/'><motion.h3  data-aos="fade-up" data-aos-duration="1500" whileHover={{color:'yellow'}}  transition={{duration:1 ,repeat:Infinity}} style={{marginTop:'2rem',color:'lightgreen'}}>Tourism Website</motion.h3></NavLink>
+      <NavLink to='https://tourism-web-with-react-by-vicky.vercel.app/'><motion.h3  data-aos="fade-up" data-aos-duration="1500" whileHover={{color:'yellow'}}  transition={{duration:1 ,repeat:Infinity}} style={{marginTop:'2rem',color:'lightgreen'}}><u>Tourism Website</u></motion.h3></NavLink>
      <NavLink to='https://tourism-web-with-react-by-vicky.vercel.app/'><img data-aos="fade-up" data-aos-duration="1500" src='buddha.jpg' className='img-thumbnail' style={{marginTop:'3rem',height:'20rem',width:'30rem',boxShadow:'1px 1px 2px black, 0 0 25px white, 0 0 5px white'}} alt='....'/></NavLink>
      
     </div>
@@ -97,7 +98,7 @@ export default function Home() {
     <div class="col">
     <div class="row">
     <div class="col" >
-      <NavLink to='https://typing-web-by-vicky.netlify.app/'><motion.h3  whileHover={{color:'yellow'}} data-aos="fade-up" data-aos-duration="1500" transition={{duration:1 ,repeat:Infinity}} style={{marginTop:'2rem',color:'lightgreen'}}>Typing Website</motion.h3></NavLink>
+      <NavLink to='https://typing-web-by-vicky.netlify.app/'><motion.h3  whileHover={{color:'yellow'}} data-aos="fade-up" data-aos-duration="1500" transition={{duration:1 ,repeat:Infinity}} style={{marginTop:'2rem',color:'lightgreen'}}><u>Typing Website</u></motion.h3></NavLink>
      <NavLink to='https://typing-web-by-vicky.netlify.app/'><img data-aos="fade-up" data-aos-duration="1500" src='typist.jpg' className='img-thumbnail' style={{marginTop:'3rem',height:'20rem',boxShadow:'1px 1px 2px black, 0 0 25px white, 0 0 5px white'}} alt='....'/></NavLink>
      
       <div className="col" style={{marginTop:'1rem'}}>
@@ -111,7 +112,7 @@ export default function Home() {
 
     <div class="row">
     <div class="col" >
-      <motion.h3 data-aos="fade-up" data-aos-duration="1500" whileHover={{color:'yellow'}}  transition={{duration:1 ,repeat:Infinity}} style={{marginTop:'2rem',color:'lightgreen'}} onClick={handleclick}>portfolio Website</motion.h3>
+      <motion.h3 data-aos="fade-up" data-aos-duration="1500" whileHover={{color:'yellow'}}  transition={{duration:1 ,repeat:Infinity}} style={{marginTop:'2rem',color:'lightgreen'}} onClick={handleclick}><u>portfolio Website</u></motion.h3>
      <img data-aos="fade-up" data-aos-duration="1500" src='portfool.jpg' className='img-thumbnail' style={{marginTop:'3rem',height:'20rem',boxShadow:'1px 1px 2px black, 0 0 25px white, 0 0 5px white'}} alt='....' onClick={handleclick}/>
      
       <div className="col" style={{marginTop:'1rem'}}>
