c49bc22fb459fce6454e5b055f65a46c8b12b73c
vicky kumar mishra <mishravicky0141@gmail.com>
2023-09-24 18:44:30 +0530

    Update Index.js

--- My-portfolio-website-using-React-js/myportfolio/src/schemas/Index.js
+++ My-portfolio-website-using-React-js/myportfolio/src/schemas/Index.js
@@ -2,7 +2,7 @@ import * as Yup from "yup";
 
 export const signUpSchema = Yup.object({
   name: Yup.string().min(2).max(25).required("Please enter your name"),
-  mobile:Yup.number().min(10).max(10).required("Please enter your mobile no"),
+  mobile:Yup.string().matches(/^\d{10}$/, 'Mobile number must be 10 digits').required("Please enter your mobile no"),
   useremail: Yup.string().email().required("Please enter your email"),
   message: Yup.string().min(10).max(60).required("Please enter your message"),
   
