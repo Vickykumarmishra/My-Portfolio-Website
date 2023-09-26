a8c5247899e3a55fe4cc203f389a122b2410f034
vicky kumar mishra <mishravicky0141@gmail.com>
2023-09-26 14:19:47 +0530

    Update Projects.js

--- My-portfolio-website-using-React-js/myportfolio/src/components/Projects.js
+++ My-portfolio-website-using-React-js/myportfolio/src/components/Projects.js
@@ -63,7 +63,7 @@ export default function Projects() {
     <h5 className="card-title" style={{color:"darkgreen"}}><b>Portfolio Website</b></h5>
     <b><p>Techstack:- React js,Javascript,Emailjs,Html and css</p></b>
     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <NavLink to="#" className="btn btn-primary" onClick={handleclick}>Click Here</NavLink>
+    <a href="#" className="btn btn-primary" onClick={handleclick}>Click Here</a>
   
   </div>
 </div>
